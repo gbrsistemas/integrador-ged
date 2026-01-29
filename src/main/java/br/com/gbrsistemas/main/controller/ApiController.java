fc955c0028ffc85c3ee86ca25f52a2c13c8e3dc6
daianeGBRsistemas <daiane@gbrsistemas.com.br>
2026-01-29 15:31:19 -0300

    conferencia api

--- src/main/java/br/com/gbrsistemas/main/controller/ApiController.java
+++ src/main/java/br/com/gbrsistemas/main/controller/ApiController.java
@@ -14,6 +14,7 @@ import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import com.google.gson.Gson;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import javax.ws.rs.core.Form;
@@ -79,19 +80,27 @@ public class ApiController {
         Client client = null;
         try {
             client = ClientBuilder.newClient();
-    
-            WebTarget target = client.target(this.api + "/crvirtual-demandas/vistoria/pesquisar-vistorias-efetuadas");
+
+            String url = this.api + "/crvirtual-demandas/vistoria/pesquisar-vistorias-efetuadas";
+            System.out.println("\n\n\n URL CFM: " + url + "\n\n\n");
+            WebTarget target = client.target(url);
     
             String requestBody = JsonConverter.objectToJson(vistoriaEfetuadaSeletorRequest);
+            System.out.println("\n\n\n requestBody: " + requestBody + "\n\n\n");
     
             Response response = target
                     .request(MediaType.APPLICATION_JSON)
                     .header("Authorization", "Bearer " + token)
                     .post(Entity.json(requestBody));
-                            
+
+            System.out.println("\n\n\n STATUS: " + response.getStatus() + "\n\n\n");
             if (response.getStatus() == 200) {
                 VistoriaEfetuadaResponseDTO vistoriaResponse = JsonConverter.jsonToObject(response.readEntity(String.class), VistoriaEfetuadaResponseDTO.class);
-                
+                System.out.println("\n\n\n TOTAL: " + vistoriaResponse.getTotal() + "\n\n\n");
+
+                Gson gson = new Gson();
+                System.out.println("\n\n\n " + gson.toJson(vistoriaResponse.getItens()) + "\n\n\n");
+
                 return vistoriaResponse;
             } else {
                 System.err.println("Erro na solicitação. Código de resposta: " + response.getStatus());
