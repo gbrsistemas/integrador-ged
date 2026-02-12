e21341717534560500402868387354077ec19b9b
daianeGBRsistemas <daiane@gbrsistemas.com.br>
2026-02-12 16:01:13 -0300

    atualizar demanda

--- src/main/java/br/com/gbrsistemas/main/dto/DemandaExternaDto.java
+++ src/main/java/br/com/gbrsistemas/main/dto/DemandaExternaDto.java
@@ -5,15 +5,33 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class DemandaExternaDto {
 
-    private Integer idDemanda;
+    private Integer numeroDemanda;
+    private Integer anoDemanda;
+    private String ufDemanda;
     private Integer idSituacao;
 
-    public Integer getIdDemanda() {
-        return idDemanda;
+    public Integer getNumeroDemanda() {
+        return numeroDemanda;
     }
 
-    public void setIdDemanda(Integer idDemanda) {
-        this.idDemanda = idDemanda;
+    public void setNumeroDemanda(Integer numeroDemanda) {
+        this.numeroDemanda = numeroDemanda;
+    }
+
+    public Integer getAnoDemanda() {
+        return anoDemanda;
+    }
+
+    public void setAnoDemanda(Integer anoDemanda) {
+        this.anoDemanda = anoDemanda;
+    }
+
+    public String getUfDemanda() {
+        return ufDemanda;
+    }
+
+    public void setUfDemanda(String ufDemanda) {
+        this.ufDemanda = ufDemanda;
     }
 
     public Integer getIdSituacao() {
